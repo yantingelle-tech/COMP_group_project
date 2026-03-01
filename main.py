685c541e93aec1c4742b45b18de3e7e0bb6069c8
yantingelle-tech <yantingelle@gmail.com>
2026-03-02 01:12:45 +0800

    Create Clothing class with attributes
    Fix typo in constructor and add clothing attributes.

---
+++ main.py
@@ -0,0 +1,8 @@
+class Clothing:
+  def __inint__(self, name, c_type, color, season, style, meterial):
+    self.name = name  #like "black jeans"
+    self.c_type = c_type  #like jeans, tops, shoes
+    self.color = color  #
+    self.season = season
+    self.material = material
+    self.is_clean = true
