2f546e56d8a5b8343a0ef61441082292a381fb4b
yantingelle-tech <yantingelle@gmail.com>
2026-03-03 18:12:25 +0800

    Fix constructor initialization in Clothing class

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Clothing:
   def __inint__(self, name, c_type, color, season, style, meterial):
     self.name = name  #like "black jeans"
-    self.c_type = c_type  #like jeans, tops, shoes
-    self.color = color  #
-    self.season = season
-    self.material = material
-    self.is_clean = true
+    self.c_type = c_type  #like "jeans", "tops", "shoes"
+    self.color = color  #like "black", "white", "blue"
+    self.season = season  #like "spring", "summer", "autumn", "winter"
+    self.material = material  #like "casual", "formal", "sporty"
+    self.is_clean = true  #default the clothes as clean
